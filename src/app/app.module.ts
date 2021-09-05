ad6a7ac17e59342ce812540b90357d362e3f0324
Melania <m_fondevilla@hotmail.com>
2021-09-05 20:41:20 +0200

    clase 56

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeroesModule } from './heroes/heroes.module';
 import { ContadorModule } from './contador/contador/contador.module';
+import { DbzModule } from './dbz/dbz.module';
 //crear un módulo llamado contadorModule
 //declaracions y exportaciones
 
@@ -14,7 +15,8 @@ import { ContadorModule } from './contador/contador/contador.module';
   imports: [
     BrowserModule,
     HeroesModule,
-    ContadorModule
+    ContadorModule,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
